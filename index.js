7d62f088125ac27f267bd11e71805ff50f2867a3
cafedeichi <ichiro.hirata.biz@gmail.com>
2020-11-06 13:16:28 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const core = require('@actions/core');
+const Nightmare = require('nightmare');
+const Path = require('path');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const configuration = {
+    show: false,
+    width: 320,
+    height: 568
+}
+const userUrl = 'https://www.apple.com/support/systemstatus/';
+const userFilePath = 'status/userServiceStatus.txt';
+const developerUrl = 'https://developer.apple.com/system-status/';
+const developerFilePath = 'status/developerServiceStatus.txt';
+
+Nightmare(configuration)
+    .goto(userUrl)
+    .wait('body')
+    .evaluate(() => document.querySelector('body').innerHTML)
+    .end()
+    .then( response => {
+        let result = getData(response);
+        write(userFilePath, result);
+        core.setOutput('system_status', '<' + userUrl + '|*System Status:*>\n' + result);
+    }).catch(error => {
+        core.setFailed(`Data Fetching failed with error ${error}`);
+    });
+
+Nightmare(configuration)
+    .goto(developerUrl)
+    .wait('body')
+    .evaluate(() => document.querySelector('body').innerHTML)
+    .end()
+    .then( response => {
+        let result = getData(response);
+        write(developerFilePath, result);
+        core.setOutput('developer_system_status', '<' + developerUrl + '|*Developer System Status:*>\n' + result);
+    }).catch(error => {
+        core.setFailed(`Data Fetching failed with error ${error}`);
+    });
+
+function getData(html) {
+    try {
+        const $ = cheerio.load(html);
+        const events = $('.event-container').last().children('.event');
+        let issues = '';
+
+        events.each((index, element) => {
+            let status = '';
+            if ($(element).children().hasClass('event-title')) {
+                status = $(element).first().children('.event-title')
+            } else {
+                status = $(element)
+            }
+            status = status.text().replace(' - (Opens in new window)','');
+            console.log(status);
+            if (!status.match('Available') && !status.match('Resolved')) {
+                issues = issues + '- ' + status + '\n';
+            }
+        })
+
+        let result = '';
+        if(issues.length == 0){
+            result = 'All services are operating normally.'
+        } else {
+            result = 'Some issues detected.\n' + issues;
+        }
+        return result;
+
+    } catch(error) {
+        core.setFailed(`Scraping failed with error ${error}`);
+    }
+}
+
+function write(filePath, stream) {
+    try {
+        fs.writeFileSync(Path.join(__dirname, filePath), stream, { flag: 'w+' });
+    } catch(error) {
+        core.setFailed(`Creating file failed with error ${error}`);
+    }
+}
