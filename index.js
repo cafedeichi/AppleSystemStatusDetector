ce4ea884526362fc33b64af37ed6d1e2c493141d
cafedeichi <ichiro.hirata.biz@gmail.com>
2022-03-01 13:16:32 +0900

    feat: Add `@here` mention for Slack when detected outage regarding App Store.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const userUrl = 'https://www.apple.com/support/systemstatus/';
 const userFilePath = 'status/userServiceStatus.txt';
 const developerUrl = 'https://developer.apple.com/system-status/';
 const developerFilePath = 'status/developerServiceStatus.txt';
+let isAppStoreOutage = false;
 
 Nightmare(configuration)
     .goto(userUrl)
@@ -22,6 +23,7 @@ Nightmare(configuration)
     .then( response => {
         let result = getData(response);
         write(userFilePath, result);
+        core.setOutput('mention', isAppStoreOutage ? '@here\n' : '');
         core.setOutput('system_status', '<' + userUrl + '|*System Status:*>\n' + result);
     }).catch(error => {
         core.setFailed(`Data Fetching failed with error ${error}`);
@@ -35,6 +37,7 @@ Nightmare(configuration)
     .then( response => {
         let result = getData(response);
         write(developerFilePath, result);
+        core.setOutput('mention', isAppStoreOutage ? '@here\n' : '');
         core.setOutput('developer_system_status', '<' + developerUrl + '|*Developer System Status:*>\n' + result);
     }).catch(error => {
         core.setFailed(`Data Fetching failed with error ${error}`);
@@ -61,6 +64,9 @@ function getData(html) {
                 } else if (status.match('issue')) {
                     issues = issues + ':large_yellow_circle: ' + serviceWithComment.text() + '\n';
                 } else if (status.match('outage')) {
+                    if (serviceWithComment.text().indexOf('App Store') == 0) {
+                        isAppStoreOutage = true;
+                    }
                     issues = issues + ':red_circle: ' + serviceWithComment.text() + '\n';
                 } else if (status.match('maintenance')) {
                     issues = issues + ':white_circle: ' + serviceWithComment.text() + '\n';
